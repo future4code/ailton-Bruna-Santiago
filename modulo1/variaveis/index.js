33e4fd9c876ca69a16bf082c393581305ad90149
Bruna Zoais Santiago <brunazohh@gmail.com>
2022-05-09 15:17:29 -0300

    aula variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+// let nome
+// let idade
+// console.log(typeof nome, typeof idade)
+/*RESPONDER*/
+
+// let nome = prompt ("Qual é o seu nome")
+// let idade = +prompt ("Qual é sua idade")
+// console.log("Olá", nome, "Você tem", idade, "anos")
+
+// let pergunta1= ("Você está feliz hoje?")
+// let pergunta2= ("Você está triste hoje?")
+// let pergunta3= ("Você está com raiva hoje?")
+// let resposta1= "Não"
+// let resposta2= "Sim"
+// let resposta3= "Não"
+// console.log(`${pergunta1} ${resposta1}, ${pergunta2} ${resposta2} ${pergunta3} ${resposta3}`)
+
+let a= 10
+let b= 25
+let c
+c=a
+a=b
+b=c
+console.log(a,b)
+
+
