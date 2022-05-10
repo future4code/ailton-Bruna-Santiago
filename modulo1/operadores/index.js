e48877c0b468ee1d3b08cf5cb69b503df7dfd8a9
Bruna Zoais Santiago <brunazohh@gmail.com>
2022-05-10 17:36:40 -0300

    aula de operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,70 @@
+// Exercícios de interpretação de código
+
+// // 1 - 
+// a) false
+// b) false
+// c) false
+// d) undefined
+
+
+// 2 - Foi impresso concatenado, colocar "Number" antes de prompt.
+
+// 3 - Abaixo sugestão para a soma dos 2 números:
+
+// let primeiroNumero = 1
+// let segundoNumero = 1
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+// Exercícios de escrita de código
+
+// 1 -
+
+// const minhaIdade= Number(prompt("Qual é a sua idade?"))
+// const idadeDoSeuMelhorAmigo= Number(prompt("Qual é a idade do seu melhor amigo?"))
+// const diferencaDeIdade= minhaIdade > idadeDoSeuMelhorAmigo
+// const difIdade= minhaIdade - idadeDoSeuMelhorAmigo
+
+// console.log (`Sua idade é ${minhaIdade} anos a idade do seu amigo é ${idadeDoSeuMelhorAmigo} anos, eu sou mais velho? ${diferencaDeIdade},a diferença é ${difIdade} anos`)
+
+// 2 -
+
+// const valor1= Number(prompt("Digite um número par"))
+
+
+// const resultado = valor1 % 2
+
+// console.log(`${valor1} o resultado é ${resultado} se for 0 é par, se não for é impar`)
+
+// COMENTAR:
+
+// 3 -
+// const idade= Number(prompt("Qual sua idade em anos?"))
+// const meses= idade * 12
+// const dias= idade *365
+// const horas= idade *8760
+
+// console.log(`A idade em anos é ${idade}, a idade em meses é ${meses} a idade em dias é ${dias} a idade em horas é ${horas}`)
+
+// 4 -
+// const numero1= Number(prompt("Qual o numero 1"))
+// const numero2= Number(prompt("Qual o numero 2"))
+
+// const resultado1= numero1 > numero2
+// const resultado2= numero1 === numero2
+// const resultado3= (numero1 % numero2) === 0
+// const resultado4= (numero2 % numero1) === 0
+ 
+// console.log(resultado1, resultado2, resultado3, resultado4)
+
+
+
+
+
+
+
+
+
+
