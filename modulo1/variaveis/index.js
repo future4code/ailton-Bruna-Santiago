d03947810f57f71f81df1dc3c6c343183d6d0e0b
Bruna Zoais Santiago <brunazohh@gmail.com>
2022-05-09 22:36:56 -0300

    alterando variaveis

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,32 +1,40 @@
+// let a= 10
+// let b=10
+
+// console.log(b)
 
 
+// b= 5
+// console.log(a, b)
 
+// let p= prompt("Quantas horas você trabalha por dia?")
+// let t= prompt("Quanto você recebe por dia?")
 
-
+// console.log(`Você recebe ${1t/2}por hora`)
 
 // let nome
 // let idade
 // console.log(typeof nome, typeof idade)
-/*RESPONDER*/
+// /*RESPONDER*/
 
 // let nome = prompt ("Qual é o seu nome")
 // let idade = +prompt ("Qual é sua idade")
 // console.log("Olá", nome, "Você tem", idade, "anos")
 
-// let pergunta1= ("Você está feliz hoje?")
-// let pergunta2= ("Você está triste hoje?")
-// let pergunta3= ("Você está com raiva hoje?")
-// let resposta1= "Não"
-// let resposta2= "Sim"
-// let resposta3= "Não"
-// console.log(`${pergunta1} ${resposta1}, ${pergunta2} ${resposta2} ${pergunta3} ${resposta3}`)
+let pergunta1= ("Você está feliz hoje?")
+let pergunta2= ("Você está triste hoje?")
+let pergunta3= ("Você está com raiva hoje?")
+let resposta1= "Não"
+let resposta2= "Sim"
+let resposta3= "Não"
+console.log(`${pergunta1} ${resposta1}. ${pergunta2} ${resposta2}. ${pergunta3} ${resposta3}`)
 
-let a= 10
-let b= 25
-let c
-c=a
-a=b
-b=c
-console.log(a,b)
+// let a= 10
+// let b= 25
+// let c
+// c=a
+// a=b
+// b=c
+// console.log(a,b)
 
 
