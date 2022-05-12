0884721522fc8f25486696e6e6d2249fb5ea48d9
Bruna Zoais Santiago <brunazohh@gmail.com>
2022-05-12 19:46:05 -0300

    aula funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,86 @@
+// Exercícios de interpretação de código
+
+// 1 - Leia o código abaixo
+
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// minhaFuncao(2)
+// minhaFuncao(10)
+
+// a) O que vai ser impresso no console?
+// // 10
+//    50
+
+
+// b) O que aconteceria se retirasse os dois console.log e simplesmente invocasse a função minhaFuncao(2) e minhaFuncao(10)? O que apareceria no console?
+//
+
+// 2 - Leia o código abaixo
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+// a) Explique o que essa função faz e qual é sua utilidade
+// R: Dá a frase em minusculas e procura a palavra cenoura no texto
+
+
+// b) Determine qual será a saída no console para cada uma das 3 entradas do usuário:
+// . Eu gosto de cenoura //true
+// . CENOURA é bom pra vista // true
+// . Cenouras crescem na terra // true
+
+
+//Exercícios de escrita de código
+
+// 1 - Escreva as funções explicadas abaixo:
+
+// a)
+
+// let nome= "Bruna"
+// let idade= 33
+// let cidade= "Parisi"
+// let estudante= "estudante"
+
+// function imprimirMensagem() {
+//     console.log(
+//         `Eu sou ${ nome}, tenho ${idade} anos, moro em ${cidade} e sou ${estudante}`)
+//     }
+//     imprimirMensagem()
+
+    // b)
+
+    //  function infoPessoa(nome, idade, cidade, profissao){
+    //      console.log(`Eu sou ${nome}, tenho ${idade} moro em ${cidade} e sou ${profissao}`)
+    //  }
+
+    //  infoPessoa("Lais", 23, "São Paulo", "instrutora")
+
+     // 2 - 
+// a)
+//      const a = 10
+//     const b = 5
+
+//     function soma(a, b) {
+//     let soma = a + b
+//     return soma
+// }
+
+// const resultado= soma(a, b)
+// console.log(resultado)
+
+// b)
+
+// const a= 5
+// const b= 5
+
+// function maiorOuIgual(a, b)
+
+
